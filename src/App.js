cc438c908ea4b595d46a6696d484e05155b23fa1
Endless Hypnosis <endlesshypnosis@gmail.com>
2017-08-15 18:06:43 -0600

    cards showing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import DistrictList from './DistrictList'
+
+import DistrictRepository from './helper.js';
+import kinderData from '../data/kindergartners_in_full_day_program.js';
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      districts: []
+    }
+  }
+
+  componentDidMount() {
+    const districtRepo = new DistrictRepository(kinderData);
+    this.setState({
+      districts: districtRepo.findAllMatches('colo')
+    })
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div className="App">
+        <DistrictList districts={ this.state.districts } />
+
+      </div>
     );
   }
 }
---
+++ src/District.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const District = ({ location, data }) => {
+  return (
+    <div>
+      <h2>{ location }</h2>
+    </div>
+  )
+}
+
+export default District
