64d3123abc2b8407ba12b350407163c9057884e9
Endless Hypnosis <endlesshypnosis@gmail.com>
2017-08-18 19:00:45 -0600

    clickable districts

--- src/District.js
+++ src/District.js
@@ -1,30 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/District.css';
 
 
-const District = ({ location, average, data }) => {
+export default class District extends Component {
+  constructor() {
+    super();
+    this.state = {
+      shouldIExpand: false,
+      shouldICompare: false
+    }
+  }
 
-  const years = Object.keys(data);
+  handleClick() {
+    // console.log('it was clicked');
+    this.setState({
+      shouldIExpand: !this.state.shouldIExpand
+    }, () => {
+      console.log('afterClick:', this.state.shouldIExpand);
+    })
+  }
 
-  return (
-    <div className="card">
-      <div className="card-header">
-          <p className="info-back">{ location }</p>
-          <div className="info-rank-wrapper">
-            <p className="info-rank">Avg</p>
-            <p className="info-rank-sub">{ average }</p>
-          </div>
-      </div>
-      <div className="card-bio">
-        { years.map((year, i) =>
-            <div key={i}>
-              <p>{year} : {data[year]}</p>
+  componentDidMount() {
+    this.cardBio.style.display = 'none'
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('component will update:', nextState.shouldIExpand);
+    if (nextState.shouldIExpand) {
+      this.cardBio.style.display = 'flex'
+    } else {
+      this.cardBio.style.display = 'none'
+    }
+  }
+
+  render() {
+
+    // const stylez = {
+    //   display: 'none'
+    // }
+
+    const { location, average, data } = this.props;
+
+    const years = Object.keys(data);
+
+    return (
+      <div className="card">
+        <div  className="card-header"
+              onClick={this.handleClick.bind(this)}
+              ref={(element) => this.cardHeader = element}
+        >
+            <p className="info-back">{ location }</p>
+            <div className="info-rank-wrapper">
+              <p className="info-rank">Avg</p>
+              <p className="info-rank-sub">{ average }</p>
             </div>
-          )
-        }
+        </div>
+        <div  className="card-bio"
+              ref={(element) => this.cardBio = element}
+        >
+          { years.map((year, i) =>
+              <div key={i}>
+                <p>{year} : {data[year]}</p>
+              </div>
+            )
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default District
