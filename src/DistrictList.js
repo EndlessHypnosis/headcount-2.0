1847c41891706b9409dfc8ed2b5d3bef54862ac9
Endless Hypnosis <endlesshypnosis@gmail.com>
2017-08-20 19:15:58 -0600

    props for search and district list done

--- src/DistrictList.js
+++ src/DistrictList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import District from './District';
+import PropTypes from 'prop-types';
 import '../styles/DistrictList.css';
 
 const DistrictList = ({ districts, addDistrictToShowDown, districtShowDown }) => {
@@ -24,6 +25,28 @@ const DistrictList = ({ districts, addDistrictToShowDown, districtShowDown }) =>
   )
 
 
+}
+
+// I'm getting a warning in my console about duplicate key...is it not
+// good to keep passing through props as the same name everywhere?
+DistrictList.propTypes = {
+  addDistrictToShowDown: PropTypes.func,
+
+  districts: PropTypes.array,
+  districts: PropTypes.arrayOf(PropTypes.object),
+  districts: PropTypes.arrayOf(PropTypes.shape({
+    average: PropTypes.string,
+    location: PropTypes.string,
+    data: PropTypes.object
+  })),
+
+  districtShowDown: PropTypes.array,
+  districtShowDown: PropTypes.arrayOf(PropTypes.object),
+  districtShowDown: PropTypes.arrayOf(PropTypes.shape({
+    average: PropTypes.string,
+    location: PropTypes.string,
+    data: PropTypes.object
+  }))
 }
 
 export default DistrictList
